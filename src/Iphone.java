41496786634858958625685327bc811b9a19d35f
ArielaFontes <arielashomo@gmail.com>
2024-06-09 16:33:39 -0300

    Iphone

--- src/Iphone.java
+++ src/Iphone.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Iphone {
+    public static void main(String args[]) {
+
+        System.out.println("Telefone Ligado");
+
+        ReprodutorMusical musica = new ReprodutorMusical();
+        musica.tocar();
+        musica.pausar();
+        musica.selecionarMusica();
+
+        System.out.println("Chamada");
+
+        AparelhoTelefonico telefone = new AparelhoTelefonico();
+        telefone.ligar();
+        telefone.atender();
+        telefone.iniciarCorreioVoz();
+
+        System.out.println("Iniciando Navegador");
+
+        NavegadorInternet navegador = new NavegadorInternet();
+        navegador.exibirPagina();
+        navegador.adicionarNovaAba();
+        navegador.AtualizarPagina();
     }
 }
